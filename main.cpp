fc10b5b56f4a0f8d120351617f31426a78d03600
kyolau <kyoshirolong@163.com>
2021-12-15 23:34:35 +0800

    20211215-2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-15 21:27:30
- * @LastEditTime: 2021-12-15 22:51:06
+ * @LastEditTime: 2021-12-15 23:34:18
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /lesson3/main.cpp
@@ -15,6 +15,7 @@ int  main(int argc,char **argv)
     int b = 20;
     int c = 30;
     int d = 40;
+    int e = 50;
     printf("a = %d, b = %d ,c = %d \n",a,b,c);
     return 0;
 }
